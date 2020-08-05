2ef61261d9c7a025b7e422cd39db76aae7d2d43d
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-08-05 19:47:37 +0700

    add bottom menu

--- app/src/main/java/com/example/ternakburung/MainActivity.java
+++ app/src/main/java/com/example/ternakburung/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.ternakburung;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView rvCategory;
     private ArrayList<Category> list = new ArrayList<>();
 
+//    bottom navigation
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +37,26 @@ public class MainActivity extends AppCompatActivity {
 
         list.addAll(BirdData.getListData());
         showRecyclerList();
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    return false;
+                }
+            };
 
     private void showRecyclerList() {
         rvCategory.setLayoutManager(new LinearLayoutManager(this));
         ListCategoryAdapter listCategoryAdapter = new ListCategoryAdapter(list);
         rvCategory.setAdapter(listCategoryAdapter);
     }
+
+
+
 }
